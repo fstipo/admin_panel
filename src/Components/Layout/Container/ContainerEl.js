d36d78861d3516bbc45ece158bdecdce2dbf9892
fstipo <fstipanov@gmail.com>
2022-03-28 16:25:09 +0200

    add sidebar

--- src/Components/Layout/Container/ContainerEl.js
+++ src/Components/Layout/Container/ContainerEl.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Sidebar from '../Sidebar/Sidebar';
 
 const ContainerEl = () => {
   return (
     <Container fluid className="main__container">
       <Row>
-        <Col className="section__sidebar col-2">1 of 1</Col>
+        <Col className="section__sidebar col-2 bg-dark ">
+          <Sidebar />
+        </Col>
         <Col className="section__main">2of2</Col>
       </Row>
     </Container>
